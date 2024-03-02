ef9ee8930b1263bcb300c50ec3a5d385689ee0bc
kushansewmina2468 <155358924+kushansewmina2468@users.noreply.github.com>
2024-03-02 20:02:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,18 +59,18 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./sessions`)
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("❄ꜱᴛᴀʀᴛ ᴡɪᴛʜ ᴄᴏᴜɴᴛʀʏ ᴄᴏᴅᴇ ᴏꜰ ʏᴏᴜʀ ᴡʜᴀᴛꜱᴀᴘᴘ ɴᴜᴍʙᴇʀ❄(ᴇxᴀᴍᴘʟᴇ)= +94774041805 ")))
+            console.log(chalk.bgBlack(chalk.redBright("❄ꜱᴛᴀʀᴛ ᴡɪᴛʜ ᴄᴏᴜɴᴛʀʏ ᴄᴏᴅᴇ ᴏꜰ ʏᴏᴜʀ ᴡʜᴀᴛꜱᴀᴘᴘ ɴᴜᴍʙᴇʀ❄(ᴇxᴀᴍᴘʟᴇ)= +94772108460 ")))
             process.exit(0)
          }
       } else {
-         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`💝PLEASE TYPE YOUR WHATSAPP NUMBER 💝\nFOR EXAMPLE🩷 = +94774071805  : `)))
+         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`💝PLEASE TYPE YOUR WHATSAPP NUMBER 💝\nFOR EXAMPLE🩷 = +94772108460  : `)))
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          // Ask again when entering the wrong number
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("❄ꜱᴛᴀʀᴛ ᴡɪᴛʜ ᴄᴏᴜɴᴛʀʏ ᴄᴏᴅᴇ ᴏꜰ ʏᴏᴜʀ ᴡʜᴀᴛꜱᴀᴘᴘ ɴᴜᴍʙᴇʀ❄(ᴇxᴀᴍᴘʟᴇ)= +94774041805 ")))
+            console.log(chalk.bgBlack(chalk.redBright("❄ꜱᴛᴀʀᴛ ᴡɪᴛʜ ᴄᴏᴜɴᴛʀʏ ᴄᴏᴅᴇ ᴏꜰ ʏᴏᴜʀ ᴡʜᴀᴛꜱᴀᴘᴘ ɴᴜᴍʙᴇʀ❄(ᴇxᴀᴍᴘʟᴇ)= +94772108460 ")))
 
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`💝PLEASE TYPE YOUR WHATSAPP NUMBER 💝\nFOR EXAMPLE🩷 = +94774071805  :`)))
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`💝PLEASE TYPE YOUR WHATSAPP NUMBER 💝\nFOR EXAMPLE🩷 = +94772108460  :`)))
             phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
             rl.close()
          }
@@ -95,23 +95,28 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./sessions`)
            await delay(800)
           const output = await axios.post('http://paste.c-net.org/',`${btoa(sessionXeon)}`, {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }});
           let c = output.data.split('/')[3]
-           await XeonBotInc.sendMessage(XeonBotInc.user.id, {text: 'DarkNero;;;'+c});	
-          let cc = `⚠⃟⃪⃬⃖⛩ᴅᴏ ɴᴏᴛ ꜱʜᴀʀᴇ ᴛʜɪꜱ ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴘʟᴇᴀꜱᴇ ᴅᴏɴᴛ ꜱʜᴀʀᴇ.⛩⃟⃪⃬⃖⚠
-          _____________
-          
-          🎀ᴛʜᴀɴᴋꜱ ꜰʀᴏᴍ ᴜꜱɪɴɢ ᴏᴜʀ ᴘᴜʙʟɪᴄ ʙᴏᴛ ꜱᴇʀᴠɪᴄᴇ🎀
-          _____________
-          
-          ʏᴏᴜʀ Qʀ ꜱᴄᴀɴɴᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ✅
-          _____________
-          
-          💝ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ .ᴘʟᴇᴀꜱᴇ ᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴅᴀʀᴋ ɴᴇʀᴏ ᴜᴘᴅᴀᴛᴇ ᴅᴇᴛᴀɪʟꜱ💝=https://chat.whatsapp.com/K7i6DvdR8u34WCFuASM26a
-          _____________
-          
-          ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʏʙᴇʀ ʏᴀᴋᴜᴢᴀ ᴛᴇᴀᴍ💎
-          _____________
-          ©ᴅᴀʀᴋ ɴᴇʀᴏ ᴠ9🎀
-          `
+           await XeonBotInc.sendMessage(XeonBotInc.user.id, {text: 'DARKSHAN;;;'+c});	
+          let cc = `┏┅┉⃝┅┅┅┅⃟┅◂ ◃ ◉ ▹ ▸┅⃟┅┅┅┅⃝┅┅┓
+
+╟ ♤ 𝚃𝙷𝙰𝙽𝙺𝚂 𝙵𝙾𝚁 𝙲𝙷𝙾𝙾𝚂𝙴 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙽 ┋𝙼𝙳 
+┋
+╟ ♤ 𝙳𝙴𝚅𝙰𝙻𝙾𝙿𝙴𝚁 𝙱𝚈 𝙺𝚄𝚂𝙷𝙰𝙽 𝚂𝙴𝚆𝙼𝙸𝙽𝙰 
+┋
+╟ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙱𝙾𝚃 𝙲𝚁𝙴𝙰𝚃𝙴 𝚅𝙸𝙳𝙴𝙾
+┋https://youtube.com/@darkshanyt1
+
+╟ ♤ 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙲𝙷𝙰𝙽𝙽𝙴𝙻
+┋https://whatsapp.com/channel/0029VaFLAgi90x2oD70Hwq1z
+╟ ♤ 𝙽𝙾𝚃𝙴
+┋𝙳𝙾𝙽'𝚃 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝚈𝙾𝚄 𝚂𝙴𝚂𝚂𝙸𝙾𝙽_𝙸𝙳 ┋ 𝚃𝙾  𝙰𝙽𝚈𝙾𝙽𝙴 𝙾𝚃𝙷𝙴𝚁𝚆𝙸𝚂𝙴 𝚃𝙷𝙰𝚃 𝙲𝙰𝙽 ┋𝙰𝙲𝙲𝙴𝚂𝚂 𝙲𝙷𝙰𝚃𝚂
+
+
+◎┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅◎
+                         𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙽 𝙼𝙳
+◎┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅◎
+
+
+┗┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┛`
           await XeonBotInc.sendMessage(XeonBotInc.user.id, {text: cc});
 
 
